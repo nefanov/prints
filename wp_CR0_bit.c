cc61d9d60db4985f915f1d52982705bb180d55ff
nefanov <kolya-efan@mail.ru>
2016-11-16 14:26:20 +0400

    Create wp_CR0_bit.c

---
+++ wp_CR0_bit.c
@@ -0,0 +1,31 @@
+/*
+This function sets WP bit to zero to allow writing into a memory pages of Linux Kernel;
+*/
+
+static inline unsigned long native_pax_open_kernel(void)
+{
+    unsigned long cr0;
+
+    preempt_disable();
+    barrier();
+    cr0 = read_cr0() ^ X86_CR0_WP;
+    BUG_ON(unlikely(cr0 & X86_CR0_WP));
+    write_cr0(cr0);
+    return cr0 ^ X86_CR0_WP;
+}
+
+/*
+This function restores WP bit to protect Linux Kernel pages from writing;
+*/
+
+static inline unsigned long native_pax_close_kernel(void)
+{
+    unsigned long cr0;
+
+    cr0 = read_cr0() ^ X86_CR0_WP;
+    BUG_ON(unlikely(!(cr0 & X86_CR0_WP)));
+    write_cr0(cr0);
+    barrier();
+    preempt_enable_no_resched();
+    return cr0 ^ X86_CR0_WP;
+}
